ab3c1764f16d4a2b403c47b4d886465f75b969b2
Phung Ly <98497119+pkly1219@users.noreply.github.com>
2023-10-08 22:48:55 -0700

    Update SimulationDriver.java

--- Assignment1_3560/src/SimulationDriver.java
+++ Assignment1_3560/src/SimulationDriver.java
@@ -1,5 +1,4 @@
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 import java.util.*;
 import java.util.List;
 
@@ -8,30 +7,41 @@ class SimulationDriver {
     public static void main(String[] args) {
         VotingService votingService = new VotingService();
 
+        //Create a list of single-choice and multiple-choice question
         List<Question> questions = new ArrayList<>();
+        //Multiple-choice questions
         questions.add(new Question("What are your favorite places?",
-                Arrays.asList("New York", "San Jose", "Houston", "San Diego")));
-        questions.add(new Question("Which human activity is the largest contributor to the increase in atmospheric carbon dioxide levels?",
-                Arrays.asList("Deforestation", "Burning fossil fuels", "Agriculture", "Industrial waste")));
+                Arrays.asList("A. New York", "B. San Jose", "C. Houston", "D. San Diego"), true));
+        questions.add(new Question("What countries are in Asia?",
+                Arrays.asList("A. Korea", "B. India", "C. New Zealand", "D. Egypt"), true));
+
+        //Single-choice questions
+        questions.add(new Question("Which human activity is the largest contributor to " +
+                "the increase in atmospheric carbon dioxide levels?",
+                Arrays.asList("A. Deforestation", "B. Burning fossil fuels", "C. Agriculture", "D. Industrial waste"), false));
+        questions.add(new Question("Which one is the capital of Canada?",
+                Arrays.asList("A. Ottawa", "B. Vancouver", "C. Ontario", "D. Quebec"), false));
 
         Random rand = new Random();
+
+        //Random generate the number of student
+        int numStudents = rand.nextInt(95) + 5;
+        System.out.println("The number of student is: "+ numStudents);
 
         for (Question question : questions) {
             votingService.configureQuestion(question);
 
-            int numStudents = rand.nextInt(95) + 5;
-            System.out.println("The number of student is: "+ numStudents);
             for (int i = 1; i <= numStudents; i++) {
                 Student student = new Student("Student" + i);
 
                 // Simulate single-choice or multiple-choice answers
-                if (rand.nextBoolean()) {
-                    student.submitAnswer(rand.nextInt(question.getOptions().size()));
+                if (!question.isMultipleChoice()) {
+                    student.submitAnswer(rand.nextInt(question.getChoice().size()));
                 } else {
                     List<Integer> answerIndices = new ArrayList<>();
-                    int numChoices = rand.nextInt(question.getOptions().size()) + 1;
+                    int numChoices = rand.nextInt(question.getChoice().size()) + 1;
                     for (int j = 0; j < numChoices; j++) {
-                        answerIndices.add(rand.nextInt(question.getOptions().size()));
+                        answerIndices.add(rand.nextInt(question.getChoice().size()));
                     }
                     student.submitAnswers(answerIndices);
                 }
@@ -39,7 +49,7 @@ class SimulationDriver {
                 votingService.acceptSubmission(student);
             }
 
-            votingService.outputStatistics();
+            votingService.displayResult();
         }
     }
 }
